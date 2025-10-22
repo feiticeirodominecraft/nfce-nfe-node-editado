8063ec80ede00cc87d955b6b711390c65fe5a31d
feiticeirodominecraft <114190155+feiticeirodominecraft@users.noreply.github.com>
2025-10-22 09:06:43 -0300

    Delete lib directory

--- lib/domain/use-cases/processo/consulta-recibo.d.ts
+++
@@ -1,3 +0,0 @@
-import { type Configuracoes, type SoapRequest } from '../../../domain/contracts/repos';
-import { type RetornoNF } from '../../../domain/contracts/repos/retorno';
-export declare function consultaRecibo(response: SoapRequest.Output, uf: string, configuracoes: Configuracoes): Promise<RetornoNF>;
--- lib/domain/use-cases/processo/status.d.ts
+++
@@ -1,3 +0,0 @@
-import { type Configuracoes, type TCodUfIbge } from '../../../domain/contracts/repos';
-import { type RetornoStatusServico } from '../../../domain/contracts/repos/retorno';
-export declare function statusServico(configuracoes: Configuracoes, cUf: TCodUfIbge): Promise<RetornoStatusServico>;
